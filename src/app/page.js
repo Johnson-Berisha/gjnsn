715a7771c964ece39f4f0a0f3808ceaed866b8d8
Gjonson <gjonsonberisha@gmail.com>
2026-01-28 19:39:30 +0100

    made header

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="container measure">
-      <h1>Gjonson Berisha</h1>
+      <header>
+        <div class="logo">Johnson Berisha</div>
+        <nav>
+          <Link href="#">Work</Link>
+          <Link href="#">About</Link>
+          <Link href="#">Notes</Link>
+        </nav>
+      </header>
+      <section className="hero">
+        <h1>Web Developer crafting digital experiences with clarity and purpose.</h1>
+        <p className="hero-sub">Currently based in Kosovo. Focusing on beautiful UI and UX.</p>
+      </section>
     </div>
   );
 }
